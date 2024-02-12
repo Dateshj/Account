677d24a0a461c70ece9b3afd1e3c3374ce447b9c
Devikakhunt9 <104673812+Devikakhunt9@users.noreply.github.com>
2024-02-13 00:36:31 +0530

    by dk

--- Accountapp/Accountapp/obj/Debug/net6.0/Accountapp.AssemblyInfo.cs
+++ Accountapp/Accountapp/obj/Debug/net6.0/Accountapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Accountapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91204176a94b4fb1b7c7741d14e3b2ec8b84e3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Accountapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Accountapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
