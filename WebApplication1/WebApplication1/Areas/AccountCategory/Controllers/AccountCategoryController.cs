71bc7a2fc1d12120b30122caed95bff06401cb3a
Dateshj <21010101080@darshan.ac.in>
2024-02-13 10:19:38 +0530

    1 by dj

---
+++ WebApplication1/WebApplication1/Areas/AccountCategory/Controllers/AccountCategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Areas.AccountCategory.Controllers
+{
+    //[CheckAccess]
+    [Area("AccountCategory")]
+    [Route("AccountCategory/[controller]/[action]")]
+    public class AccountCategoryController : Controller
+    {
+        public IActionResult AccountCategoryListPage()
+        {
+            return View();
+        }
+    }
+}
